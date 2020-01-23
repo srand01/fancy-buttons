c8de856a10601c559d4c1646434be32830497b70
srand01 <44007192+srand01@users.noreply.github.com>
2020-01-23 14:00:05 -0400

    updated parseGuess function

--- battleship.js
+++ battleship.js
@@ -35,43 +35,6 @@ var model = {
       if (index >= 0) {
         ship.hits[index] = "hit";
         view.displayHit(guess);
-        view.displayMessage("HIT!");var view = {
-  displayMessage: function(msg) {
-    var messageArea = document.getElementById("messageArea");
-    messageArea.innerHTML = msg;
-  },
-  displayHit: function(location) {
-    var cell = document.getElementById(location);
-    cell.setAttribute("class", "hit");
-  },
-  displayMiss: function(location) {
-    var cell = document.getElementById(location);
-    cell.setAttribute("class", "miss");
-  }
-};
-
-var model = {
-  boardSize: 7,
-  numShips: 3,
-  shipLength: 3,
-  shipsSunk: 0,
-
-  ships: [
-    { locations: ["06", "16", "26"], hits: ["", "", ""] },
-    { locations: ["24", "34", "44"], hits: ["", "", ""] },
-    { locations: ["10", "11", "12"], hits: ["", "", ""] }
-  ],
-
-  fire: function(guess) {
-    for (var i = 0; i < this.numShips; i++) {
-      //getting the ship
-      var ship = this.ships[i];
-
-      var index = ship.locations.indexOf(guess);
-      //check if index is a hit
-      if (index >= 0) {
-        ship.hits[index] = "hit";
-        view.displayHit(guess);
         view.displayMessage("HIT!");
         if (this.isSunk(ship)) {
           view.displayMessage("You sank my battleship!");
@@ -105,40 +68,19 @@ function parseGuess(guess) {
   if (guess === null || guess.shipLength !== 2) {
     alert("Oo, please enter a letter and a number on the board");
   } else {
-    firstChart = guess.chartA(0);
+    firstChart = guess.charAt(0);
     var row = alphabet.indexOf(firstChar);
-  }
-}
+    var column = guess.charAt(1);
 
-        if (this.isSunk(ship)) {
-          view.displayMessage("You sank my battleship!");
-          this.shipsSunk++;
-        }
-        return true;
-      }
+    if (isNaN(row) || isNaN(column)) {
+      alert("Oops, that isn't on the board.");
+    } else if (
+      row < 0 ||
+      row >= model.boardSize ||
+      column < 0 ||
+      column >= model.boardSize
+    ) {
+      alert("Oops, that's off the board!");
     }
-    view.displayMiss(guess);
-    view.displayMessage("You missed.");
-    return false;
-  },
-  isSunk: function(ship) {
-    for (var i = 0; i < this.shipLength; i++) {
-      if (ship.hits[i] !== "hit") {
-        return false;
-      }
-    }
-    return true;
-  }
-};
-
-var controller = {
-  guesses: 0,
-  processGuess: function(guess) {}
-};
-
-//parseGuess function
-function parseGuess(guess) {
-  if (guess === null || guess.shipLength !== 2) {
-    alert("Oo, please enter a letter and a number on the board");
   }
 }
