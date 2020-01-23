1f3d3cc1fde212f997bdb5d481ed067e0c7be01a
srand01 <44007192+srand01@users.noreply.github.com>
2020-01-22 23:23:01 -0400

    updated object

--- battleship.js
+++ battleship.js
@@ -35,7 +35,81 @@ var model = {
       if (index >= 0) {
         ship.hits[index] = "hit";
         view.displayHit(guess);
+        view.displayMessage("HIT!");var view = {
+  displayMessage: function(msg) {
+    var messageArea = document.getElementById("messageArea");
+    messageArea.innerHTML = msg;
+  },
+  displayHit: function(location) {
+    var cell = document.getElementById(location);
+    cell.setAttribute("class", "hit");
+  },
+  displayMiss: function(location) {
+    var cell = document.getElementById(location);
+    cell.setAttribute("class", "miss");
+  }
+};
+
+var model = {
+  boardSize: 7,
+  numShips: 3,
+  shipLength: 3,
+  shipsSunk: 0,
+
+  ships: [
+    { locations: ["06", "16", "26"], hits: ["", "", ""] },
+    { locations: ["24", "34", "44"], hits: ["", "", ""] },
+    { locations: ["10", "11", "12"], hits: ["", "", ""] }
+  ],
+
+  fire: function(guess) {
+    for (var i = 0; i < this.numShips; i++) {
+      //getting the ship
+      var ship = this.ships[i];
+
+      var index = ship.locations.indexOf(guess);
+      //check if index is a hit
+      if (index >= 0) {
+        ship.hits[index] = "hit";
+        view.displayHit(guess);
         view.displayMessage("HIT!");
+        if (this.isSunk(ship)) {
+          view.displayMessage("You sank my battleship!");
+          this.shipsSunk++;
+        }
+        return true;
+      }
+    }
+    view.displayMiss(guess);
+    view.displayMessage("You missed.");
+    return false;
+  },
+  isSunk: function(ship) {
+    for (var i = 0; i < this.shipLength; i++) {
+      if (ship.hits[i] !== "hit") {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+var controller = {
+  guesses: 0,
+  processGuess: function(guess) {}
+};
+
+//parseGuess function
+function parseGuess(guess) {
+  var alphabet = ["A", "B", "C", "D", "E", "F", "G"];
+  if (guess === null || guess.shipLength !== 2) {
+    alert("Oo, please enter a letter and a number on the board");
+  } else {
+    firstChart = guess.chartA(0);
+    var row = alphabet.indexOf(firstChar);
+  }
+}
+
         if (this.isSunk(ship)) {
           view.displayMessage("You sank my battleship!");
           this.shipsSunk++;
