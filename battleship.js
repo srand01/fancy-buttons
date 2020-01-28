191b38ef7808cb51902458ab60e4cbb8c340fd86
srand01 <44007192+srand01@users.noreply.github.com>
2020-01-27 23:55:55 -0400

    updated generateShip method

--- battleship.js
+++ battleship.js
@@ -66,7 +66,16 @@ var model = {
       }
     }
   },
-  generateShip: function() {}
+  generateShip: function() {
+    var direction = Math.floor(Math.random() * 2);
+    var row, col;
+
+    if (direction === 1){
+
+    } else{
+      
+    }
+  }
 };
 
 var controller = {
