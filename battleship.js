36a3882f92bbc0e3d9f6769183a790f0cdb53b3b
srand01 <44007192+srand01@users.noreply.github.com>
2020-01-29 11:09:54 -0400

    initialized ship locations with 0

--- battleship.js
+++ battleship.js
@@ -20,9 +20,9 @@ var model = {
   shipsSunk: 0,
 
   ships: [
-    { locations: ["06", "16", "26"], hits: ["", "", ""] },
-    { locations: ["24", "34", "44"], hits: ["", "", ""] },
-    { locations: ["10", "11", "12"], hits: ["", "", ""] }
+    { locations: [0, 0, 0], hits: ["", "", ""] },
+    { locations: [0, 0, 0], hits: ["", "", ""] },
+    { locations: [0, 0, 0], hits: ["", "", ""] }
   ],
 
   fire: function(guess) {
